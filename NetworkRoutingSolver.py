4a0880a52c0c1827969ef5e04f5f66f3d6f4b17a
Jacob Wright <jacob.wright.ut@gmail.com>
2022-10-15 23:47:45 -0600

    FEAT: array implementation except djikstras

--- NetworkRoutingSolver.py
+++ NetworkRoutingSolver.py
@@ -9,20 +9,26 @@ class NetworkRoutingSolver:
     def __init__( self):
         pass
 
+    pqArray = []
+    pqHeap = []
+
     def initializeNetwork( self, network ):
         assert( type(network) == CS312Graph )
         self.network = network
 
     def getShortestPath( self, destIndex ):
         self.dest = destIndex
+
         # TODO: RETURN THE SHORTEST PATH FOR destIndex
         #       INSTEAD OF THE DUMMY SET OF EDGES BELOW
         #       IT'S JUST AN EXAMPLE OF THE FORMAT YOU'LL 
         #       NEED TO USE
+
         path_edges = []
         total_length = 0
         node = self.network.nodes[self.source]
         edges_left = 3
+
         while edges_left > 0:
             edge = node.neighbors[2]
             path_edges.append( (edge.src.loc, edge.dest.loc, '{:.0f}'.format(edge.length)) )
@@ -34,9 +40,50 @@ class NetworkRoutingSolver:
     def computeShortestPaths( self, srcIndex, use_heap=False ):
         self.source = srcIndex
         t1 = time.time()
-        # TODO: RUN DIJKSTRA'S TO DETERMINE SHORTEST PATHS.
-        #       ALSO, STORE THE RESULTS FOR THE SUBSEQUENT
-        #       CALL TO getShortestPath(dest_index)
+
+        if use_heap:
+            self.djikstraHeap(srcIndex)
+        else:
+            self.djikstraArray(srcIndex)
+
         t2 = time.time()
         return (t2-t1)
 
+
+########## Array Implementation of Priority Queue ##########
+
+    def djikstraArray(self, srcIndex):
+        self.makeQueueArray()
+        self.decreaseKeyArray(srcIndex, 0)
+        
+        
+
+    def makeQueueArray(self):
+        for node in self.network.nodes:
+            self.pqArray.append(node)
+            self.pqArray[node.node_id].distance = float('inf')
+        for node in self.pqArray:
+            print(node)
+            print(node.distance)
+
+
+    def insertArray(self, node, distance):
+        self.pqArray[node] = distance
+
+    def deleteMinArray(self):
+        minNode = None
+        minDistance = float('inf')
+        for node in self.pqArray:
+            if self.pqArray[node] is not None and self.pqArray[node] < minDistance:
+                minNode = node
+                minDistance = self.pqArray[node]
+        
+        self.pqArray.pop(minNode)
+        return minNode
+
+    def decreaseKeyArray(self, nodeIndex, distance):
+        self.pqArray[nodeIndex].distance = distance
+        print(self.pqArray[nodeIndex])
+        print(self.pqArray[nodeIndex].distance)
+
+    
